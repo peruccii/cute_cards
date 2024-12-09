e5b9380182c33129a901f205d071f6aa472cea1f
peruccii <peruccii9217@hotmail.com>
2024-12-08 21:09:18 -0300

    completing invite-spec tests

--- src/application/entities/invite.spec.ts
+++ src/application/entities/invite.spec.ts
@@ -1,6 +1,7 @@
 import { makeInvite } from "@test/factories/invite-factory"
-import { Email } from "./fieldsValidations/email"
 import { UrlMusic } from "./fieldsValidations/url_music"
+import { PhotoLimitExceeded } from "@application/usecases/errors/photo-limit-exceeded"
+import { InvitePlan } from "./enums/invitePlan"
 
 describe('INVITE TEST', () => {
     it('should be able to create a invite', () => {
@@ -12,5 +13,21 @@ describe('INVITE TEST', () => {
         expect(() => {
             makeInvite({ url_music: new UrlMusic('@') })
         }).toThrow(Error)
+    })
+
+    it('should not be able to create invite with more photos than invite plan BASIC require', async () => {
+        const invite = makeInvite({ imageUrls: new Array(8).fill('') })
+
+        expect(() => {
+            invite.verifyQuantityOfPhothosByInvitePlan(invite.invite_plan, invite.imageUrls);
+        }).toThrow(PhotoLimitExceeded)
+    })
+
+    it('should not be able to create invite with more photos than invite plan PREMIUM require', async () => {
+        const invite = makeInvite({ invite_plan: InvitePlan.PREMIUM, imageUrls: new Array(8).fill('') })
+
+        expect(() => {
+            invite.verifyQuantityOfPhothosByInvitePlan(invite.invite_plan, invite.imageUrls);
+        }).toThrow(PhotoLimitExceeded)
     })
 })
