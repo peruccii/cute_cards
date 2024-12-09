71b81271f9437d86deb4b5bc1fce32a679e67247
peruccii <peruccii2917@hotmail.com>
2024-12-08 23:24:25 -0300

    add config service to .env variables

--- src/application/usecases/handle-events-stripe.ts
+++ src/application/usecases/handle-events-stripe.ts
@@ -4,17 +4,17 @@ import Stripe from "stripe";
 
 @Injectable()
 export class HandleEventsStripe {
-    private stripe;
+    private stripe: Stripe;
 
     constructor(private configService: ConfigService ) {
-        this.stripe = new Stripe(process.env.STRIPE_API_KEY!, {
+        this.stripe = new Stripe(this.configService.get('STRIPE_API_KEY')!, {
             apiVersion: '2024-11-20.acacia'
         })
     }
 
     verifyEvent(request: any): Stripe.Event {
         const signature = request.headers['stripe-signature'];
-        const endpointSecret = process.env.WHSEC_STRIPE;
+        const endpointSecret = process.env.WHSEC_STRIPE! // Todo: maybe alternate to this.config.service;
 
         return this.stripe.webhooks.constructEvent(
             request.rawBody,
@@ -25,6 +25,6 @@ export class HandleEventsStripe {
 
     async handleSessionCompleted(event: Stripe.Event) {
         const session = event.data.object as Stripe.Checkout.Session;
-        console.log(`Evento de sessão completada: ${session.id}}`);
+        console.log(`session completed ${session.id}}`);
     }
 }
