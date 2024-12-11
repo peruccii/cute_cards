01a124a56c427c3ed9f8f43ceb273fc4e47038b3
peruccii <peruccii2917@hotmail.com>
2024-12-11 16:32:57 -0300

    creating a CreateEmailResponse as Resend follow to create

--- src/application/repositories/mail-repository.ts
+++ src/application/repositories/mail-repository.ts
@@ -1,5 +1,7 @@
 import { Email } from "@application/entities/fieldsValidations/email";
 
+export interface CreateEmailResponse {}
+
 export abstract class MailRepository {
-    abstract sendEmail(email: Email): Promise<void>;
+    abstract sendEmail(email: Email): Promise<CreateEmailResponse>;
 }
