8c881cc3132169c448af4b4c723bf0042c8347cb
peruccii <peruccii2917@hotmail.com>
2024-12-11 16:33:50 -0300

    handle if send email fail

--- src/application/usecases/mail/resend-mail.service.ts
+++ src/application/usecases/mail/resend-mail.service.ts
@@ -3,22 +3,25 @@ import { MailRepository } from "@application/repositories/mail-repository";
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { Resend } from "resend";
+import { ResendSendMailError } from "../errors/resend-send-email-error";
 
 @Injectable()
 export class Resendmail implements MailRepository {
 
-    private resend
+    private resend: Resend
 
     constructor(private configService: ConfigService) {
         this.resend = new Resend(this.configService.get('RESEND_KEY'));
     }
 
     async sendEmail(email: Email) {
-        await this.resend.emails.send({
+        const response = await this.resend.emails.send({
             from: 'Acme <onboarding@resend.dev>',
-            to: [email],
+            to: [email.value],
             subject: 'hello world',
             html: '<p>it works!</p>',
         });
+        if (!response.data?.id) throw new ResendSendMailError();
+        return response
     }
 }
