aa7b9990c7b29f504f64fbd222f64193855ce83e
peruccii <peruccii2917@hotmail.com>
2024-12-12 19:44:03 -0300

    send email on checkout.session.completed event

--- src/application/usecases/handle-events-stripe.ts
+++ src/application/usecases/handle-events-stripe.ts
@@ -1,31 +1,42 @@
-import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import Stripe from "stripe";
+import { Email } from '@application/entities/fieldsValidations/email';
+import { CreateSendEmailRequest, MailRepository } from '@application/repositories/mail-repository';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
 
 @Injectable()
 export class HandleEventsStripe {
     private stripe: Stripe;
 
-    constructor(private configService: ConfigService ) {
+    constructor(private configService: ConfigService, private mailRepository: MailRepository) {
         this.stripe = new Stripe(this.configService.get('STRIPE_API_KEY')!, {
-            apiVersion: '2024-11-20.acacia'
-        })
+            apiVersion: '2024-11-20.acacia',
+        });
+        this.mailRepository = mailRepository;
     }
 
     verifyEvent(request: any): Stripe.Event {
         const signature = request.headers['stripe-signature'];
-        const endpointSecret = this.configService.get('WHSEC_STRIPE')!
+        const endpointSecret = this.configService.get('WHSEC_STRIPE')!;
 
         return this.stripe.webhooks.constructEvent(
             request.rawBody,
             signature,
-            endpointSecret
+            endpointSecret,
         );
     }
 
     async handleSessionCompleted(event: Stripe.Event) {
+
         const session = event.data.object as Stripe.Checkout.Session;
-        console.log(`session completed ${session.id}}`);
-        return 'session completed'
+
+        const request: CreateSendEmailRequest = {
+            email: new Email(session.customer_email!),
+            inviteType: session.metadata!.inviteType,
+            clientName: session.customer_details?.name!,
+            inviteId: session.metadata!.inviteId
+        }
+
+        if (session.customer_email) await this.mailRepository.sendEmail(request)
     }
 }
