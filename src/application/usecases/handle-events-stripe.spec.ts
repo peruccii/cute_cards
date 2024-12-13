3bc7e4cd3d009e2c3dd0c1a45abe3337b3c112d0
peruccii <peruccii2917@hotmail.com>
2024-12-13 14:41:16 -0300

    creating mock repository to handle events test

--- src/application/usecases/handle-events-stripe.spec.ts
+++ src/application/usecases/handle-events-stripe.spec.ts
@@ -8,18 +8,20 @@ import Stripe from 'stripe';
 describe('HANDLE EVENTS STRIPE TEST', () => {
     let handle_events: HandleEventsStripe;
 
+    // simulando sendEmail do repository que me devolve um id
     const mailRepositoryMock = {
-        sendEmail: jest.fn().mockResolvedValue({ id: 'test-id' }),
+        // o nome do metodo tem que existir, pois na hora que o repo real passar a ser o repo falso os metodos tem que serem o mesmo
+        sendEmail: jest.fn().mockResolvedValue({ id: 'test-id' }), // o retorno tambem tem que ser o mesmo
     };
 
     beforeEach(async () => {
         dotenv.config();
         const module: TestingModule = await Test.createTestingModule({
             providers: [
-                HandleEventsStripe,
+                HandleEventsStripe, // vai passar a ter o "mailRepositoryMock" como repository
                 {
-                    provide: MailRepository,
-                    useValue: mailRepositoryMock,
+                    provide: MailRepository, // providando o repository real para o repository falso ( mailRepositoryMock )
+                    useValue: mailRepositoryMock, // repository falso
                 },
                 {
                     provide: ConfigService,
