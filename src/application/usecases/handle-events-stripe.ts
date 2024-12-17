483cc84093155e697f7f670cd6d8a78452b47705
peruccii <peruccii2917@hotmail.com>
2024-12-17 19:26:11 -0300

    creating a invite create event emmiter

--- src/application/usecases/handle-events-stripe.ts
+++ src/application/usecases/handle-events-stripe.ts
@@ -1,10 +1,18 @@
+import { InvitePlan } from '@application/entities/enums/invitePlan';
+import { InviteType } from '@application/entities/enums/inviteType';
 import { Email } from '@application/entities/fieldsValidations/email';
+import { Message } from '@application/entities/fieldsValidations/message';
+import { SubTitle } from '@application/entities/fieldsValidations/subTitle';
+import { Title } from '@application/entities/fieldsValidations/title';
+import { InviteProps } from '@application/entities/invite';
+import { InvitePlanDetails } from '@application/entities/invite-plan-details';
 import {
   CreateSendEmailRequest,
   MailRepository,
 } from '@application/repositories/mail-repository';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 import Stripe from 'stripe';
 
 @Injectable()
@@ -14,6 +22,7 @@ export class HandleEventsStripe {
   constructor(
     private configService: ConfigService,
     private mailRepository: MailRepository,
+    private eventEmmiter: EventEmitter2,
   ) {
     this.stripe = new Stripe(this.configService.get('STRIPE_API_KEY')!, {
       apiVersion: '2024-11-20.acacia',
@@ -35,17 +44,36 @@ export class HandleEventsStripe {
   async handleSessionCompleted(event: Stripe.Event) {
     const session = event.data.object as Stripe.Checkout.Session;
 
+    const metadata = session.metadata as Partial<InviteProps>;
+
     const request: CreateSendEmailRequest = {
       email: new Email(session.customer_email!),
-      inviteType: session.metadata!.inviteType,
+      inviteType: session.metadata!.invite_type,
       clientName: session.customer_details!.name!,
       inviteId: session.metadata!.inviteId,
     };
 
+    const invite: InviteProps = {
+      email: new Email(session.customer_email!),
+      date: metadata.date!,
+      duration_invite: InvitePlanDetails.getDate(
+        metadata.invite_plan as InvitePlan,
+      ),
+      title: new Title(''),
+      imageUrls: [''],
+      invite_plan: metadata.invite_plan as InvitePlan,
+      message: new Message(''),
+      sub_title: new SubTitle(''),
+      url_music: metadata.url_music ? metadata.url_music : null,
+      invite_type: metadata.invite_type as InviteType,
+    };
+
+    this.eventEmmiter.emit('invite.created', invite);
+
     return await this.mailRepository.sendEmail(request);
   }
 
-  async handleSessionCancelled() {
+  async handleSessionExpired() {
     // delete folder firebase
   }
 }
