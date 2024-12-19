582eea61339b58d6400c45b0a13badb8d9f80152
peruccii <peruccii2917@hotmail.com>
2024-12-18 22:42:30 -0300

    event emitteer

--- src/application/usecases/handle-events-stripe.ts
+++ src/application/usecases/handle-events-stripe.ts
@@ -1,9 +1,6 @@
 import { InvitePlan } from '@application/entities/enums/invitePlan';
 import { InviteType } from '@application/entities/enums/inviteType';
 import { Email } from '@application/entities/fieldsValidations/email';
-import { Message } from '@application/entities/fieldsValidations/message';
-import { SubTitle } from '@application/entities/fieldsValidations/subTitle';
-import { Title } from '@application/entities/fieldsValidations/title';
 import { InviteProps } from '@application/entities/invite';
 import { InvitePlanDetails } from '@application/entities/invite-plan-details';
 import {
@@ -14,6 +11,7 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import Stripe from 'stripe';
+import { Metadata } from './create-checkout';
 
 @Injectable()
 export class HandleEventsStripe {
@@ -28,6 +26,7 @@ export class HandleEventsStripe {
       apiVersion: '2024-11-20.acacia',
     });
     this.mailRepository = mailRepository;
+    this.eventEmmiter = eventEmmiter;
   }
 
   verifyEvent(request: any): Stripe.Event {
@@ -44,31 +43,31 @@ export class HandleEventsStripe {
   async handleSessionCompleted(event: Stripe.Event) {
     const session = event.data.object as Stripe.Checkout.Session;
 
-    const metadata = session.metadata as Partial<InviteProps>;
+    const metadata = session.metadata as unknown as Metadata;
 
     const request: CreateSendEmailRequest = {
       email: new Email(session.customer_email!),
-      inviteType: session.metadata!.invite_type,
+      inviteType: metadata.invite_type,
       clientName: session.customer_details!.name!,
-      inviteId: session.metadata!.inviteId,
+      inviteId: metadata.id,
     };
 
     const invite: InviteProps = {
       email: new Email(session.customer_email!),
-      date: metadata.date!,
+      date: metadata.date,
       duration_invite: InvitePlanDetails.getDate(
         metadata.invite_plan as InvitePlan,
       ),
-      title: new Title(''),
-      imageUrls: [''],
+      title: metadata.title,
+      imageUrls: [],
       invite_plan: metadata.invite_plan as InvitePlan,
-      message: new Message(''),
-      sub_title: new SubTitle(''),
+      message: metadata.message,
+      sub_title: metadata.sub_title,
       url_music: metadata.url_music ? metadata.url_music : null,
       invite_type: metadata.invite_type as InviteType,
     };
 
-    this.eventEmmiter.emit('invite.created', invite);
+    this.eventEmmiter.emit('invite.created', invite); // a
 
     return await this.mailRepository.sendEmail(request);
   }
