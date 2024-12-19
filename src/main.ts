561ff74f124aef9bc59e1f5e5c8d9f4dabebe578
peruccii <peruccii2917@hotmail.com>
2024-12-19 19:29:16 -0300

    implementing rawBody / bodyParser so stripe checkout

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import type { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    rawBody: true,
+    bodyParser: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   dotenv.config();
+
+  app.useBodyParser('json', { limit: '20mb' });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
