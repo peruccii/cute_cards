234fedaad7b740f08fa253c0c83576270881192b
peruccii <peruccii2917@hotmail.com>
2024-12-20 19:22:50 -0300

    todo: change file name

---
+++ src/application/interfaces/event.ts
@@ -0,0 +1,20 @@
+import { InvitePlan } from '@application/entities/enums/invitePlan';
+import { InviteType } from '@application/entities/enums/inviteType';
+import { Email } from '@application/entities/fieldsValidations/email';
+import { Message } from '@application/entities/fieldsValidations/message';
+import { SubTitle } from '@application/entities/fieldsValidations/subTitle';
+import { Title } from '@application/entities/fieldsValidations/title';
+import { UrlMusic } from '@application/entities/fieldsValidations/url_music';
+
+export default interface PrismaCreateInviteRequest {
+  id: string;
+  email: Email;
+  invite_plan: InvitePlan;
+  invite_type: InviteType;
+  title: Title;
+  sub_title: SubTitle;
+  message: Message;
+  url_music: UrlMusic | null;
+  date: Date;
+  duration_invite: Date;
+}
