03fa6c7dc1ad427c8cea69679444a7de4d65921f
peruccii <peruccii2917@hotmail.com>
2024-12-20 19:23:36 -0300

    todo: refatorate code

--- src/application/usecases/create-checkout.ts
+++ src/application/usecases/create-checkout.ts
@@ -1,11 +1,11 @@
 import Stripe from 'stripe';
 import { CheckoutRepository } from '@application/repositories/checkout-repository';
-import { Invite, InviteProps } from '@application/entities/invite';
 import { Injectable } from '@nestjs/common';
 import { InvitePlan } from '@application/entities/enums/invitePlan';
 import { ConfigService } from '@nestjs/config';
+import CheckoutRequest from '@application/interfaces/checkout';
 
-export interface Metadata extends InviteProps {
+export interface Metadata extends CheckoutRequest {
   id: string;
 }
 
@@ -19,10 +19,25 @@ export class CreateInviteCheckoutSession implements CheckoutRepository {
     });
   }
 
-  async createCheckoutSession(invite: Invite): Promise<string | null> {
+  async createCheckoutSession(invite: CheckoutRequest): Promise<string | null> {
+    const customer = await this.stripe.customers.create({
+      metadata: {
+        id: invite.id,
+        url_music: invite.url_music || null,
+        invite_type: invite.invite_type,
+        inviteId: invite.id,
+        invite_plan: invite.invite_plan,
+        date: invite.date.toISOString(),
+        title: invite.title as unknown as string,
+        sub_title: String(invite.sub_title as unknown as string),
+        message: invite.message as unknown as string,
+      },
+    });
+
     const session = await this.stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       mode: 'payment',
+      customer: customer.id,
       line_items: [
         {
           price_data: {
@@ -30,23 +45,23 @@ export class CreateInviteCheckoutSession implements CheckoutRepository {
             product_data: {
               name: `${invite.invite_type} ${invite.invite_plan}`,
               images: ['https://example.com/image.png'],
-              metadata: {
-                id: invite.id,
-                url_music: invite.url_music?.value || null,
-                invite_type: invite.invite_type,
-                inviteId: invite.id,
-                invite_plan: invite.invite_plan,
-                date: invite.date.getDate(),
-                title: invite.title as unknown as string,
-                sub_title: String(invite.sub_title as unknown as string),
-                message: invite.message as unknown as string,
-              },
             },
             unit_amount: invite.invite_plan === InvitePlan.BASIC ? 2000 : 3000,
           },
           quantity: 1,
         },
       ],
+      metadata: {
+        id: invite.id,
+        url_music: invite.url_music || null,
+        invite_type: invite.invite_type,
+        inviteId: invite.id,
+        invite_plan: invite.invite_plan,
+        date: invite.date.toISOString(),
+        title: invite.title as unknown as string,
+        sub_title: String(invite.sub_title as unknown as string),
+        message: invite.message as unknown as string,
+      },
       success_url: 'https://example.com/success',
       cancel_url: 'https://example.com/cancel',
     });
