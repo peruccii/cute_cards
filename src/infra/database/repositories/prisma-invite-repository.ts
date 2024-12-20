73110d35b7a1bbfe952e2f0fd963ee7da3fca381
peruccii <peruccii2917@hotmail.com>
2024-12-20 19:20:14 -0300

    persisting invite on database and using event emmiter to upload invite / stripe dont accept 500 character long for imagesUrl

--- src/infra/database/repositories/prisma-invite-repository.ts
+++ src/infra/database/repositories/prisma-invite-repository.ts
@@ -2,12 +2,36 @@ import { Invite } from '@application/entities/invite';
 import { InviteRepository } from '@application/repositories/invite-repository';
 import { PrismaService } from '../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { PrismaInviteMapper } from '../prisma/mappers/prisma-invite-mappers';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 
 @Injectable()
 export class PrismaInviteRepository implements InviteRepository {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private eventEmmiter: EventEmitter2,
+  ) {}
 
-  create(invite: Invite) {
-    // TODO: CREATE WITH PRISMA
+  async create(invite: Invite): Promise<void> {
+    const raw = PrismaInviteMapper.toPrisma(invite);
+
+    const data = await this.prisma.invite.create({
+      data: { ...raw, imageUrls: [''] },
+    });
+
+    this.eventEmmiter.emit('data-invite.created');
+
+    this.eventEmmiter
+      .waitFor('imagesUrl.created')
+      .then((imageUrls: string[]) =>
+        this.uploadInvitePuttingImage(data.id, imageUrls),
+      );
+  }
+
+  async uploadInvitePuttingImage(inviteId: string, urls: string[]) {
+    return await this.prisma.invite.update({
+      where: { id: inviteId },
+      data: { imageUrls: urls },
+    });
   }
 }
