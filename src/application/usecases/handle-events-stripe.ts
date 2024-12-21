9dc9e00831f6fd33de2e8c7a0f3f0540c7d651cc
peruccii <peruccii2917@hotmail.com>
2024-12-21 13:39:21 -0300

    handle session expired logic done

--- src/application/usecases/handle-events-stripe.ts
+++ src/application/usecases/handle-events-stripe.ts
@@ -9,11 +9,12 @@ import { ConfigService } from '@nestjs/config';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import Stripe from 'stripe';
 import { Metadata } from './create-checkout';
-import PrismaCreateInviteRequest from '@application/interfaces/event';
+import PrismaCreateInviteRequest from '@application/interfaces/prismaCreateInviteRequest';
 import { Message } from '@application/entities/fieldsValidations/message';
 import { SubTitle } from '@application/entities/fieldsValidations/subTitle';
 import { UrlMusic } from '@application/entities/fieldsValidations/url_music';
 import { Title } from '@application/entities/fieldsValidations/title';
+import { FirebaseRepository } from '@application/repositories/firebase-repository';
 
 @Injectable()
 export class HandleEventsStripe {
@@ -23,6 +24,7 @@ export class HandleEventsStripe {
     private configService: ConfigService,
     private mailRepository: MailRepository,
     private eventEmmiter: EventEmitter2,
+    private firebaseMethods: FirebaseRepository,
   ) {
     this.stripe = new Stripe(this.configService.get('STRIPE_API_KEY')!, {
       apiVersion: '2024-11-20.acacia',
@@ -33,6 +35,7 @@ export class HandleEventsStripe {
 
   verifyEvent(request: any): Stripe.Event {
     const signature = request.headers['stripe-signature'];
+
     const endpointSecret = this.configService.get('WHSEC_STRIPE')!;
 
     if (!request.rawBody) {
@@ -53,18 +56,18 @@ export class HandleEventsStripe {
 
     const email = new Email(session.customer_details!.email!);
 
+    const durationInviteDate = InvitePlanDetails.getDate(metadata.invite_plan);
+
+    const url_music = metadata.url_music
+      ? new UrlMusic(metadata.url_music)
+      : null;
+
     const request: CreateSendEmailRequest = {
       email: email.value,
       inviteType: metadata.invite_type,
       clientName: session.customer_details!.name!,
       inviteId: metadata.id,
     };
-
-    const durationInviteDate = InvitePlanDetails.getDate(metadata.invite_plan);
-
-    const url_music = metadata.url_music
-      ? new UrlMusic(metadata.url_music)
-      : null;
 
     const invite: PrismaCreateInviteRequest = {
       id: metadata.id,
@@ -84,7 +87,8 @@ export class HandleEventsStripe {
     return await this.mailRepository.sendEmail(request);
   }
 
-  async handleSessionExpired() {
-    // delete folder firebase
+  async handleSessionExpired(event: Stripe.Event) {
+    const session = event.data.object as Stripe.Checkout.Session;
+    this.firebaseMethods.delete(session.customer_details!.email!);
   }
 }
