2db23f092264b3e68daf035ffcb712a4482130b7
peruccii <peruccii2917@hotmail.com>
2025-01-05 13:24:05 -0300

    todo: custom error

---
+++ src/application/usecases/get-invite.ts
@@ -0,0 +1,25 @@
+import { Invite } from '@application/entities/invite';
+import { InviteRepository } from '@application/repositories/invite-repository';
+import { Injectable } from '@nestjs/common';
+
+interface GetInviteRequest {
+  id: string;
+}
+
+interface GetInviteResponse {
+  invite: Invite;
+}
+
+@Injectable()
+export class GetInvite {
+  constructor(private inviteRepository: InviteRepository) {}
+
+  async execute(request: GetInviteRequest): Promise<GetInviteResponse> {
+    const { id } = request;
+    const invite = await this.inviteRepository.find(id);
+
+    if (!invite) throw new Error('');
+
+    return { invite };
+  }
+}
